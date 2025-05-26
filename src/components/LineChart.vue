1bb1ffdc5167bdad4e9f2b0717d6073d66146f0d
al_v1k <alekseevaalena442@gmail.com>
2025-05-26 15:02:03 +0900

    cdn

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,17 +1,16 @@
 <template>
   <VChart 
     class="chart" 
-    id="test"
+    :option="option" 
     :autoresize="true" 
-    />
-    <!-- :option="option"  -->
+  />
 </template>
 
 <script setup>
+
 import { ref } from 'vue';
-// import { use } from 'echarts/core';
+import { use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
-import * as echarts from 'echarts/core';
 import { BarChart, LineChart } from 'echarts/charts';
 import { computed } from 'vue';
 import {
@@ -25,22 +24,15 @@ const props = defineProps({
   optionSkills: Array
 });
 
-const chart = echarts.init('#test');
-chart.setOption({
+const option = computed(() => ({
   series: [{
     type: 'pie',
     data: props.optionSkills || [] // Защита от undefined
   }],
-})
-// const option = computed(() => ({
-//   series: [{
-//     type: 'pie',
-//     data: props.optionSkills || [] // Защита от undefined
-//   }],
-// }));
+}));
 
 // Регистрация необходимых компонентов
-echarts.use([
+use([
   CanvasRenderer,
   BarChart,
   LineChart,
